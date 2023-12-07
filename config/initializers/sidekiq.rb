e54232bfa0656cefc9976847366d66bd4bd74194
Collin Lewis <collinwilliamlewis@gmail.com>
2023-12-07 17:40:31 -0500

    Update Sidekiq configuration file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,17 @@
-:url: <%= ENV['REDISCLOUD_URL'] %>
+# Sidekiq configuration file (config/sidekiq.yml)
+
+# Concurrency settings
+:concurrency: 5
+
+# Redis server location
+:queues:
+  - default
+
+# Redis configuration
+production:
+  :url: <%= ENV['REDISCLOUD_URL']%>
+  
+development:
+  :url: redis://localhost:6379/0
+
+# You can add more environment specific settings if required
